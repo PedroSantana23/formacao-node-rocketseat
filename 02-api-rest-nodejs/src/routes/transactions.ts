128139699e1d38aea9ae585bf1306b5e35dcb705
PedroSantana23 <ph.silva.santana@gmail.com>
2026-05-18 18:12:47 -0300

    Módulos 1 e 2 da formação em Nodejs

---
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -0,0 +1,93 @@
+import type { FastifyInstance } from "fastify"
+import { z } from "zod"
+import { build } from "../database"
+import { randomUUID } from "node:crypto"
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists"
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.addHook('preHandler', async (request, reply) => {
+    console.log(`[${request.method}] ${request.url}`)
+  })
+
+  app.get('/', {
+    preHandler: [
+      checkSessionIdExists
+    ],
+  }, async (request, reply) => {
+    const { sessionId } = request.cookies
+
+    const transactions = await build('transactions')
+      .where('sessions_id', sessionId)
+      .select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', {
+    preHandler: [
+      checkSessionIdExists
+    ],
+  }, async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const { sessionId } = request.cookies
+
+    const transaction = await build('transactions')
+      .where({
+        id,
+        sessions_id: sessionId
+      }).first()
+
+    return { transaction }
+  })
+
+  app.get('/summary', {
+    preHandler: [
+      checkSessionIdExists
+    ],
+  }, async (request) => {
+    const { sessionId } = request.cookies
+
+    const summary = await build('transactions')
+      .where('sessions_id', sessionId)
+      .sum('amount', { as: 'amount' })
+      .first()
+
+    return { summary }
+  })
+
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit'])
+    })
+
+
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    await build('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * - 1 ,
+      sessions_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
+}
