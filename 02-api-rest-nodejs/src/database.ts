209810fe12cb8a104d07383a277702c6ae891e36
PedroSantana23 <ph.silva.santana@gmail.com>
2026-05-26 15:38:19 -0300

    Ajustes

--- 02-api-rest-nodejs/src/database.ts
+++ 02-api-rest-nodejs/src/database.ts
@@ -2,14 +2,19 @@ import type { Knex } from "knex"
 import knex from 'knex'
 import { env } from "./env"
 
+const isPostgres = env.DATABASE_CLIENT !== 'sqlite'
+
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: env.DATABASE_CLIENT === 'sqlite' ? {
-    filename: env.DATABASE_URL,
-  } : env.DATABASE_URL,
+  connection: env.DATABASE_CLIENT === 'sqlite'
+    ? { filename: env.DATABASE_URL }
+    : {
+        connectionString: env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }, // 👈 obrigatório no Render
+      },
   useNullAsDefault: true,
   migrations: {
-    extension: 'ts',
+    extension: isPostgres ? 'js' : 'ts', // 👈 js em produção
     directory: './db/migrations',
   },
 }
